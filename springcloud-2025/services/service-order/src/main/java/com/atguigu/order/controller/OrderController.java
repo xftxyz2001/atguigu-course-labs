18b8ccd079b3e7eab2909492206d437760cc3673
lfy <534096094@qq.cm>
2024-12-23 15:46:31 +0800

    配置中心 - 代码初始化

--- springcloud-2025/services/service-order/src/main/java/com/atguigu/order/controller/OrderController.java
+++ springcloud-2025/services/service-order/src/main/java/com/atguigu/order/controller/OrderController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RefreshScope
+//@RefreshScope
 @RestController
 public class OrderController {
 
