dcf592fce430b821d87ba222bd5d7f98fe47b3a6
lfy <534096094@qq.cm>
2024-12-27 16:55:19 +0800

    OpenFeign - 拦截器

--- springcloud-2025/services/service-product/src/main/java/com/atguigu/product/controller/ProductController.java
+++ springcloud-2025/services/service-product/src/main/java/com/atguigu/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.atguigu.product.controller;
 
 import com.atguigu.product.bean.Product;
 import com.atguigu.product.service.ProductService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +19,11 @@ public class ProductController {
 
     //查询商品
     @GetMapping("/product/{id}")
-    public Product getProduct(@PathVariable("id") Long productId){
-        System.out.println("hello");
+    public Product getProduct(@PathVariable("id") Long productId,
+                              HttpServletRequest request){
+
+        String header = request.getHeader("X-Token");
+        System.out.println("hello .... token=【"+header+"】");
         Product product = productService.getProductById(productId);
         return product;
     }
--- springcloud-2025/services/service-product/src/main/java/com/atguigu/product/service/impl/ProductServiceImpl.java
+++ springcloud-2025/services/service-product/src/main/java/com/atguigu/product/service/impl/ProductServiceImpl.java
@@ -17,11 +17,11 @@ public class ProductServiceImpl implements ProductService {
         product.setProductName("苹果-"+productId);
         product.setNum(2);
 
-        try {
-            TimeUnit.SECONDS.sleep(100);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            TimeUnit.SECONDS.sleep(100);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
 
         return product;
     }
