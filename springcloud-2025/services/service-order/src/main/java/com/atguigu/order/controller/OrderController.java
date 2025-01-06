9b7b86f400b130390a8493352710f3d2043fc9a5
lfy <534096094@qq.cm>
2025-01-06 19:59:20 +0800

    Gateway - 路由

--- springcloud-2025/services/service-order/src/main/java/com/atguigu/order/controller/OrderController.java
+++ springcloud-2025/services/service-order/src/main/java/com/atguigu/order/controller/OrderController.java
@@ -12,11 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
+@RequestMapping("/api/order")
 @Slf4j
 //@RefreshScope//自动刷新
 @RestController
