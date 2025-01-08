5b32b22f532f5ea0fd9bb0a22f38491fc2a5c489
lfy <534096094@qq.cm>
2025-01-08 12:46:32 +0800

    Gateway - filter - cors

--- springcloud-2025/services/service-order/src/main/java/com/atguigu/order/controller/OrderController.java
+++ springcloud-2025/services/service-order/src/main/java/com/atguigu/order/controller/OrderController.java
@@ -11,13 +11,10 @@ import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
-
+//@CrossOrigin
 //@RequestMapping("/api/order")
 @Slf4j
 //@RefreshScope//自动刷新
