f0c31c82b78b4fed83ccc49ba4ad927431a6fcf5
lfy <534096094@qq.cm>
2025-01-09 17:47:58 +0800

    seata - feign

--- springcloud-2025/services/seata-business/src/main/java/com/atguigu/business/service/impl/BusinessServiceImpl.java
+++ springcloud-2025/services/seata-business/src/main/java/com/atguigu/business/service/impl/BusinessServiceImpl.java
@@ -1,15 +1,29 @@
 package com.atguigu.business.service.impl;
 
+import com.atguigu.business.feign.OrderFeignClient;
+import com.atguigu.business.feign.StorageFeignClient;
 import com.atguigu.business.service.BusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class BusinessServiceImpl implements BusinessService {
+
+    @Autowired
+    StorageFeignClient storageFeignClient;
+
+    @Autowired
+    OrderFeignClient orderFeignClient;
+
+
+
     @Override
     public void purchase(String userId, String commodityCode, int orderCount) {
-        //TODO 1. 扣减库存
+        //1. 扣减库存
+        storageFeignClient.deduct(commodityCode, orderCount);
 
-        //TODO 2. 创建订单
+        //2. 创建订单
+        orderFeignClient.create(userId, commodityCode, orderCount);
     }
 }
--- springcloud-2025/services/seata-order/src/main/java/com/atguigu/order/SeataOrderMainApplication.java
+++ springcloud-2025/services/seata-order/src/main/java/com/atguigu/order/SeataOrderMainApplication.java
@@ -4,9 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
+
+@EnableFeignClients(basePackages = "com.atguigu.order.feign")
 @EnableTransactionManagement
 @MapperScan("com.atguigu.order.mapper")
 @EnableDiscoveryClient
